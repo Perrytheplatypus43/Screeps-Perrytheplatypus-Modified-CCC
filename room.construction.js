3d1c51c3111d386e6b50fa6a9f09c9efbc05f5f3
Athesto <alexander.i.bakalov@gmail.com>
2020-03-27 11:06:18 -0700

    ixing Issue #1 "Err with getRoomTerrain" this time with room.construction.js

--- room.construction.js
+++ room.construction.js
@@ -244,8 +244,11 @@ mod.extend = function() {
                 const structureType = layout[x] && layout[x][y];
                 const terrain = Game.map.getRoomTerrain(Game.rooms);
                 if (structureType) {
-                  if(terrain.get(pos) == TERRAIN_MASK_WALL) return failed();
-                    if (structureType === STRUCTURE_ROAD) {
+                  _.forEach(Game.rooms, room => {
+                    let terrain = Game.map.getRoomTerrain(room.name)
+                    if(terrain.get(pos) == TERRAIN_MASK_WALL) return failed();
+                  }
+                      if (structureType === STRUCTURE_ROAD) {
                         sites.push(pos);
                     } else {
                         const flagColour = constructionFlags[structureType];

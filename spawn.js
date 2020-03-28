1c6f906fdb3308ccdde971915e73598acf0b8731
Athesto <alexander.i.bakalov@gmail.com>
2020-03-28 11:30:55 -0700

    Fixing createCreep deprecation

--- spawn.js
+++ spawn.js
@@ -87,7 +87,7 @@ mod.extend = function(){
     };
     Spawn.prototype.create = function(body, name, behaviour, destiny){
         if( body.length == 0 ) return false;
-        let newName = this.spawnCreep(body, name, {memory: null, dryRun: false});
+        let newName = this.spawnCreep(body, name, {memory: null, dryRun: false, energyStructures[], directions[]});
         if( name == newName || translateErrorCode(newName) === undefined ){
             let cost = 0;
             body.forEach(function(part){

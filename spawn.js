b5acaebaed1ed5041ec95ab73b8ebee8cb0cd078
Athesto <alexander.i.bakalov@gmail.com>
2020-03-28 11:34:56 -0700

    same

--- spawn.js
+++ spawn.js
@@ -87,7 +87,7 @@ mod.extend = function(){
     };
     Spawn.prototype.create = function(body, name, behaviour, destiny){
         if( body.length == 0 ) return false;
-        let newName = this.spawnCreep(body, name, {memory: null, dryRun: false, energyStructures[], directions[]});
+        let newName = this.spawnCreep(body, name, {memory: null, dryRun: false});
         if( name == newName || translateErrorCode(newName) === undefined ){
             let cost = 0;
             body.forEach(function(part){
